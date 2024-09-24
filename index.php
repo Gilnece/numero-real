9ba466b763853f6d2115fd49f03bd2abdd5d5b2b
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2024-09-24 13:39:35 -0300

    Add Index
    pagina onde o dado será entregue para que ele possa ser analisado posteriormente

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analisador de numeros</title>
+</head>
+<body>
+    <main>
+        <h1>Analisador de dados</h1>
+        <form action="numero.php" method="post">
+            <label for="n">Numero Real: </label>
+            <input type="number" name="n" id="n" step="0.001"> 
+            <input type="submit" value="Analisar">
+        </form>
+    </main>
+</body>
+</html>
